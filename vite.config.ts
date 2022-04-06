1846c40f9cfbf09711ad5b8aa593b81f074d60fe
angel <angel.munoz@tokiota.com>
2022-04-06 08:59:54 +0200

    minify only in production mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [
+      react(),
+      visualizer(),
+      splitVendorChunkPlugin(),
+    ],
+    build: {
+      minify: mode === 'prod',
+    },
+  });
+}
