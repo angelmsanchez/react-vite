8597331ecef3fbfc8cd450d6cacbe3aa8a425786
Angel Muñoz <angelmsanchez85@gmail.com>
2023-03-29 13:03:43 +0200

    add plugin to check typescript inside Vite

--- src/home/components/index/Home.tsx
+++ src/home/components/index/Home.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Home.scss";
 import PopoverDemo from "../popover/Popover";
 
+interface PersonInterface {
+  name: string;
+  age: number;
+}
+
 export default function Home(): JSX.Element {
+  const [person, setPerson] = useState<PersonInterface>({
+    name: "Angel",
+    age: 5,
+  });
+
   return (
     <section className="home">
       <h2>Home Page</h2>
       <PopoverDemo />
+
+      <h2>Example type Vite</h2>
+      <p>Name: {person.name}</p>
+      <p>Age: {person.age}</p>
     </section>
   );
 }
